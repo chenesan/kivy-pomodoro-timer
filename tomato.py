8e4c0fd78ea108cddc1cf132cd52ba649253b913
chenesan <pipio1994@gmail.com>
2016-02-16 10:02:25 +0800

    Add datetime record

--- tomato.py
+++ tomato.py
@@ -1,7 +1,11 @@
+import datetime
+import json
+
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.properties import BooleanProperty, ObjectProperty
 from kivy.properties import NumericProperty, StringProperty
+from kivy.properties import ListProperty
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
@@ -23,9 +27,24 @@ class TomatoPlayer(Widget):
     goal_input = ObjectProperty(None)
     goal = StringProperty()
     start = BooleanProperty()
-    finished_tomato = NumericProperty(0)
+    tomato_count = NumericProperty(0)
     record = open('tomato_record.txt','a')
 
+    def __init__(self, *args, **kwargs):
+        start_time = datetime.datetime.now()
+        self._state = {
+            'date': start_time.date().strftime("%Y-%b-%d"),
+            'finished_goals': []
+        }
+        try:
+            last_state = json.load(open('pomodoro_state.json','r'))
+            if self._state['date'] == last_state['date']:
+                self._state = last_state
+        except IOError:
+            pass
+        self.tomato_count = len(self._state["finished_goals"])
+        super(TomatoPlayer, self).__init__(*args, **kwargs)
+        
     def get_time_str(self):
         h_str = "00"
         m_str = str(self.m) if self.m >= 10 else "0"+str(self.m)
@@ -42,6 +61,13 @@ class TomatoPlayer(Widget):
         self.time_strprop = "00:25:00"
         self.start = False
         self.goal_input.bind(on_text_validate=self.get_goal)
+        for i in range(self.tomato_count):
+            #Don't know why the height and width is 100*100...
+            img = Image(source='tomato.png')
+            img.center_x = 800 * (i + 1) / 10.0
+            img.center_y = 600 * 1 / 6.0
+            self.add_widget(img)
+            
 
     def update(self, t):
         if self.start:
@@ -51,37 +77,43 @@ class TomatoPlayer(Widget):
                 if self.m != -1:
                     self.s = 59
                 else: # restart clock
-                    self.finished_tomato += 1
                     self.finish_loop_handler()
             self.time_strprop = self.get_time_str()
             
     def finish_loop_handler(self):
         self.start = False
-        self.record.write("Finish today's {c} tomato: {goal}".format(
-            c=self.finished_tomato,
+        now_str = datetime.datetime.now().strftime("%Y-%b-%d, %H:%M")
+        self._state['finished_goals'].append(tuple([now_str, self.goal]))
+        self.tomato_count = len(self._state["finished_goals"])
+        self.record.write("{now}: {goal}\n".format(
+            now=now_str,
             goal=self.goal
         ))
         # TODO: Currently the it will not show the tomato icon.
-        notification = Notify.Notification.new("Times up!\n You", body=self.goal, icon="tomato.png")
+        notification = Notify.Notification.new("Times up!\n", body=self.goal, icon="tomato.png")
         notification.show()
         self.goal = ""
         self.add_widget(self.goal_input)
         img = Image(source='tomato.png')
         img.center_y = self.height * 1 / 6.0
-        img.center_x = self.width * self.finished_tomato / 10.0 
+        img.center_x = self.width * len(self._state['finished_goals']) / 10.0
+        print img.center_x, img.center_y
         self.add_widget(img)
         self.h = 0
         self.m = 25
         self.s = 0
         self.time_strprop = "00:25:00"
-        
 
+    def save_state(self, app):
+        json.dump(self._state, open('pomodoro_state.json','w'))
+        
 class TomatoApp(App):
     def build(self):
         Notify.init('tomato')
         tomato = TomatoPlayer()
         tomato.init_task()
-        Clock.schedule_interval(tomato.update, 1.0)
+        Clock.schedule_interval(tomato.update, 1.0/1000)
+        self.bind(on_stop=tomato.save_state)
         return tomato
 
 if __name__ == "__main__":
